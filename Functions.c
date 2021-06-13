1db3ae991b4e91cd2946348c23b37615a216b0d2
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-13 21:26:09 +0530

    Update Functions.c

--- Functions.c
+++ Functions.c
@@ -6,7 +6,7 @@ Question:- Simple program to show structure of function
     int main (void)
     {
         int total;
-		printf("\n Function : a simple structure of function\n");
+        printf("\n Function : a simple structure of function\n");
         total = sum (5, 6);
         printf ("The total is :  %d\n", total);
         return 0;
@@ -14,7 +14,25 @@ Question:- Simple program to show structure of function
 
     int sum (int a, int b)
     {
-	    int s;
-		s=a+b;
-        return s;
+     int s;
+     s=a+b;
+     return s;
     }
+
+Question:- Program to find the cube of any number using functions
+
+#include <stdio.h>
+double cube (double n);
+int main()
+{
+   int n;
+   double c;
+   printf("Enter any number:-");
+   scanf("%d",&n);
+   c=cube(n);
+   printf("Cube of %d is %.2f",n,c);
+   return 0;
+}
+double cube(double n){
+return(n*n*n);
+}
