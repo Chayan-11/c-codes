a5518f139f91f92cf6b98ff9c164f0375438c476
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-13 23:49:03 +0530

    Update Pattern.c

--- Pattern.c
+++ Pattern.c
@@ -124,3 +124,25 @@ Question:-
     return 0;
 }   
     
+Question:- 
+  
+        *
+      *
+    *
+  *
+*
+
+Ans:-
+
+#include <stdio.h>
+ int main()
+ {
+    int i,j;
+    for(i=5;i>=1;i--){
+      for(j=1;j<i;j++)
+        printf(" ");
+            printf("*\n");
+    }
+    return 0;
+}     
+  
