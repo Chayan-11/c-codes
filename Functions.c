23842072c356aa83fddcbdfd37b28c0e74f28d09
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-14 23:46:58 +0530

    Update Functions.c

--- Functions.c
+++ Functions.c
@@ -112,3 +112,67 @@ int abc(int p){
 return p%2;
 }
 
+Question:- C Program to Check number is Prime or Armstrong number Using User-defined Function.
+  
+#include <math.h>
+#include <stdio.h>
+
+int Prime_Number(int num);
+int Armstrong_Number(int num);
+
+int main()
+{
+   int n, flag;
+   printf("Enter a positive integer: ");
+   scanf("%d", &n);
+   flag = Prime_Number(n);
+   if (flag == 1)
+      printf("%d is a prime number.\n", n);
+   else
+      printf("%d is not a prime number.\n", n);
+   flag = Armstrong_Number(n);
+   if (flag == 1)
+      printf("%d is an Armstrong number.", n);
+   else
+      printf("%d is not an Armstrong number.", n);
+   return 0;
+}
+
+int Prime_Number(int n) {
+   int i, flag = 1, square_root;
+   square_root = sqrt(n);
+   for (i = 2; i <= square_root; ++i) {
+      if (n % i == 0) {
+         flag = 0;
+         break;
+      }
+   }
+   return flag;
+}
+
+int Armstrong_Number(int num) {
+   int originalNum, remainder, n = 0, flag;
+   double result = 0.0;
+
+   // store the number of digits of num in n
+   for (originalNum = num; originalNum != 0; ++n) {
+      originalNum /= 10;
+   }
+
+   for (originalNum = num; originalNum != 0; originalNum /= 10) {
+      remainder = originalNum % 10;
+
+      // store the sum of the power of individual digits in result
+      result += pow(remainder, n);
+   }
+
+   // condition for Armstrong number
+   if (round(result) == num)
+      flag = 1;
+   else
+      flag = 0;
+   return flag;
+}
+
+     
+     
