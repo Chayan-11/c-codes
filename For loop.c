a12410343614c4babd7c6ec91f02be6cc89a38d5
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-14 22:18:22 +0530

    Update For loop.c

--- For loop.c
+++ For loop.c
@@ -13,3 +13,19 @@ int main()
     }
     return 0;
 }
+
+#Program to input any number and find the factorial of the number using for loop.
+
+#include <stdio.h>
+int main()
+{
+   int n,f=1,i;
+   printf("Enter any number:- ");
+   scanf("%d",&n);
+   for(i=n;i>=1;i--)
+   {
+      f=f*i;
+      printf("The factorial of %d is = %d",n,f);
+   }
+    return 0;
+}
