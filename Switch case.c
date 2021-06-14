d93a3f57fbe955041a1eb1403184d52232a2ff80
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-14 20:51:29 +0530

    Update Switch case.c

--- Switch case.c
+++ Switch case.c
@@ -36,3 +36,37 @@ int main()
     }
     return 0;
 }
+
+Question:- Program to enter any two integers and find their sum, subtraction, multiplication and division depending on users choice.
+  
+  Ans:-
+    
+#include <stdio.h>
+int main()
+{
+    int m,n,a,b,c,d;
+    printf("Enter any two integers:- ");
+    scanf("%d %d",&m, &n);
+    switch(m,n)
+    {
+    case 1:
+        a=m+n;
+        printf("Sum = %d\n",a);
+        break;
+    case 2:
+        b=m-n;
+        printf("Subtraction = %d",b);
+        break;
+    case 3:
+        c=m*n;
+        printf("Multiplication = %d",c);
+        break;
+    case 4:
+        d=m/n;
+        printf("Division = %d",d);
+        break;
+    default:
+        printf("Invalid Choice");
+    }
+    return 0;
+}
