1da3f958172305141f9ab32e82663b3561f45f1b
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-14 21:17:50 +0530

    Update Switch case.c

--- Switch case.c
+++ Switch case.c
@@ -72,3 +72,51 @@ int main()
     }
     return 0;
 }
+
+Question:- program to check whether a character is vowel or consonant using switch case.
+  
+  Ans:- 
+    
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter any character:- ");
+    scanf("%c",&ch);
+    switch(ch)
+    {
+    case'A':
+        printf("Vowel");
+        break;
+    case'E':
+        printf("Vowel");
+        break;
+    case'I':
+        printf("Vowel");
+        break;
+    case'O':
+        printf("Vowel");
+        break;
+    case'U':
+        printf("Vowel");
+        break;
+    case'a':
+        printf("Vowel");
+        break;
+    case'e':
+        printf("Vowel");
+        break;
+    case'i':
+        printf("Vowel");
+        break;
+    case'o':
+        printf("Vowel");
+        break;
+    case'u':
+        printf("Vowel");
+        break;
+    default:
+        printf("Consonant");
+    }
+    return 0;
+}    
