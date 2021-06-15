d624ccd2d0ac51e2bb5115c3d7588a4ad6bbf91d
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-15 23:25:36 +0530

    Create While loop.c

---
+++ While loop.c
@@ -0,0 +1,15 @@
+#Program to print any statement for n times
+
+#include <stdio.h>
+int main()
+{
+    int n,i=1;
+    printf("Enter any integer:- ");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("It's Chayan here\n");
+        i++;
+    }
+    return 0;
+}
