bfc66d35d48baa622779ea89b1ad13ff2e66d1de
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-15 23:35:40 +0530

    Update While loop.c

--- While loop.c
+++ While loop.c
@@ -13,3 +13,24 @@ int main()
     }
     return 0;
 }
+
+#Program to print first n natural numbers in ascending and descending order
+
+#include <stdio.h>
+int main()
+{
+    int n,i=1;
+    printf("Enter any integer:- \n");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    while(n>=1)
+    {
+        printf("%d\n",n);
+        n--;
+    }
+    return 0;
+}
