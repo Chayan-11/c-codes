b9a7585ffb23f68736abcb6db670e6fb21c03795
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-17 00:59:20 +0530

    Update Pattern.c

--- Pattern.c
+++ Pattern.c
@@ -209,3 +209,33 @@ int main()
     }
     return 0;
 }  
+
+Question:-
+ 
+1
+12
+123
+1234
+12345
+
+(using while loop)  
+  
+Ans:-
+  
+#include <stdio.h>
+int main()
+{
+    int i=1,j;
+    while(i<=5)
+    {
+        j=1;
+        while(j<=i)
+        {
+            printf("%d",j);
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
