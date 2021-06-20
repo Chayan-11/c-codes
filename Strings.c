26dcea1b68aa1f3cd557c90c9fe03586c7c4ad21
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-21 02:36:09 +0530

    Update Strings.c

--- Strings.c
+++ Strings.c
@@ -90,3 +90,23 @@ int main()
    printf("\nThe number of words are:- %d\n The number of vowels are:- %d\n The number of consonants are:- %d",b+1,v,c);
     return 0;
 }
+
+#Program to input any string and print each word in different line
+
+#include <stdio.h>
+int main()
+{
+   int i;
+   char n[1000];
+   printf("Enter the string:- \n");
+   scanf("%s",n);
+   for(i=0;n[i]!='\0';i++)
+   {
+       printf("%c\n",n[i]);
+       if(n[i]==' ')
+       {
+           printf("\n");
+       }
+   }
+    return 0;
+}
