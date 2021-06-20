46a23c0080908d79cd76e879953c85f231b33743
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-21 00:59:44 +0530

    Update Strings.c

--- Strings.c
+++ Strings.c
@@ -13,7 +13,7 @@ int main()
     return 0;
 }
 
-#Program to input any string and find the length of the string without blank space 
+#Program to input any string and find the length of the string without blank spaces in case of alpha numeric and special characters
 
 #include <stdio.h>
 int main()
@@ -30,5 +30,26 @@ int main()
     b++;
 }
    printf("%d %d",l,b);
+    return 0;
+}
+
+#Program to input any string and find the length of the string without blank spaces
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   char n[100];
+   int i,lwb=0,b=0;
+   printf("Enter the string:- \n");
+   scanf("%s",n);
+   for(i=0;n[i]!='\0';i++)
+    if(n[i]==' ')
+   b++;
+   else
+    lwb++;
+
+   printf("%d %d",lwb,b);
     return 0;
 }
