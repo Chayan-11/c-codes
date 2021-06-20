09166abc439ad7da85d761dd299def390360d637
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-21 02:28:21 +0530

    Update Strings.c

--- Strings.c
+++ Strings.c
@@ -33,6 +33,22 @@ int main()
     return 0;
 }
 
+#Program to input any string and print all the characters of the string in reverse 
+
+#include <stdio.h>
+int main()
+{
+    int i,l=0;
+    char n[1000];
+    printf("Enter the string:- \n");
+    scanf("%s",n);
+    for(i=0;n[i]!='\0';i++)
+        l++;
+    for(i=l-1;i>=0;i--)
+        printf("%c",n[i]);
+    return 0;
+}
+
 #Program to input any string and find the length of the string without blank spaces
 
 #include <stdio.h>
