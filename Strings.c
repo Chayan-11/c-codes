f8112c048588efe31210eaaedf7e064e9390851c
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-21 00:42:37 +0530

    Update Strings.c

--- Strings.c
+++ Strings.c
@@ -12,3 +12,23 @@ int main()
     printf("Length of the string %s is %d",n,l);
     return 0;
 }
+
+#Program to input any string and find the length of the string without blank space 
+
+#include <stdio.h>
+int main()
+{
+   char n[100];
+   int i,l=0,b=0;
+   printf("Enter the string:- \n");
+   scanf("%s",n);
+   for(i=0;n[i]!='\0';i++){
+    if((n[i]>=65 && n[i]<=90) || (n[i]>=97 && n[i]<=122))
+   l++;
+   else
+    if(n[i]==' ')
+    b++;
+}
+   printf("%d %d",l,b);
+    return 0;
+}
