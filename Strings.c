089d3dcd972577b0d981992fb9680f74a65b6730
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-21 22:26:05 +0530

    Update Strings.c

--- Strings.c
+++ Strings.c
@@ -131,3 +131,22 @@ int main()
     d++;
    printf("The number of upper case characters are:- %d\n The number of lower case characters are:- %d\n The number of digits are:- %d",u,l,d);
 }
+
+#Program to input any string and if it's in lower case then convert it into upper case and vice-versa
+
+#include <stdio.h>
+int main()
+{
+  int i;
+  char n[1000];
+  printf("Enter the string:- \n");
+  scanf("%s",n);
+  for(i=0;n[i]!='\0';i++)
+    if(n[i]>=65 && n[i]<=90)
+    n[i]+=32;
+  else
+    if(n[i]>=97 && n[i]<=122)
+    n[i]-=32;
+  printf("%s",n);
+    return 0;
+}
