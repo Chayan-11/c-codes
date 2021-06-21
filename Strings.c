5fe10123b97fb05f5b3feb56ea51a87a12edf8f8
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-22 02:14:03 +0530

    Update Strings.c

--- Strings.c
+++ Strings.c
@@ -267,3 +267,25 @@ int main()
     printf("Frequency of %c = %d", ch,l);
     return 0;
 }
+
+#Program to input any string and check whether it is palindrome or not
+
+#include <string.h>
+int main()
+{
+    char s[1000];
+    int i,n,c=0;
+    printf("Enter  the string:- \n");
+    gets(s);
+    n=strlen(s);
+    for(i=0;i<n/2;i++)
+    {
+      if(s[i]==s[n-i-1])
+    	c++;
+ 	}
+ 	  if(c==i)
+ 	    printf("string is palindrome");
+    else
+        printf("string is not palindrome");
+    return 0;
+}
