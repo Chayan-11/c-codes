897ccf06bfb192979ee529bff169832559f6e208
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-23 00:02:20 +0530

    Update Strings.c

--- Strings.c
+++ Strings.c
@@ -382,3 +382,38 @@ int main()
     }
     return 0;
 }
+
+#Program to print the following pattern:-
+
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+    
+#include<stdio.h>
+int main()
+{
+    int n,x,y,k;
+    printf("Enter the number of rows to show the number pattern:- \n");
+    scanf("%d",&n);
+    for(x=1;x<=n;x++)
+    {
+        for(y=1;y<=n;y++)
+        {
+           if(y<=x)
+            printf("%d",y);
+           else
+            printf(" ");
+        }
+        for(y=x;y>=1;y--)
+        {
+            if(y<=x)
+                printf("%d",y);
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+       return 0;
+}    
