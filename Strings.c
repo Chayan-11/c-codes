8dce4c73177d937e1430e2853fd0f91ffe357c2a
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-22 23:52:35 +0530

    Update Strings.c

--- Strings.c
+++ Strings.c
@@ -330,3 +330,55 @@ int main()
     }
     return 0;
 }        
+
+#Program to create one hollow diamond
+
+        *
+      *   *
+     *     *
+    *       *
+   *         *
+  *           *
+ *             *
+  *           *
+   *         *
+    *       *
+     *     *
+      *   *
+        *
+
+#include <stdio.h>
+int main()
+{
+    printf("Enter how many lines to be printed:- \n");
+    int n,x,y,s=1,k;
+    scanf("%d",&n);
+    for(x=0;x<=n;x++)
+    {
+        for(y=n;y>x;y--)
+            printf(" ");
+        printf(" * ");
+        if(x>0)
+        {
+            for(k=1;k<=s;k++)
+                printf(" ");
+            s+=2;
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    s-=4;
+    for(x=0;x<=n-1;x++)
+    {
+        for(y=0;y<=x;y++)
+            printf(" ");
+        printf(" * ");
+        for(k=1;k<=s;k++)
+            printf(" ");
+        s-=2;
+        if(x!=n-1)
+            printf(" * ");
+        printf("\n");
+    }
+    return 0;
+}
