7f41c36108d42c60fd60b493fb2ab1b6199bd5cf
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-23 23:47:05 +0530

    Update Pattern.c

--- Pattern.c
+++ Pattern.c
@@ -372,3 +372,25 @@ int main()
     }
     return 0;
 }  
+
+#Program to print the floyd triangle
+ 1
+ 2 3
+ 4 5 6
+ 7 8 9 10
+   
+#include <stdio.h>
+int main()
+{
+    int i,j,k=1;
+    for(i=1;i<=5;i++)
+    {
+      for(j=1;j<=i;j++)
+      {
+          printf("%d",k);
+          k++;
+      }
+      printf("\n");
+    }
+    return 0;
+}  
