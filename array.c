de55c8a7fd51b23565cbf16470ee0adcf9409f23
Chayan Debbarma <76246106+Chayan-11@users.noreply.github.com>
2021-06-23 22:26:21 +0530

    Update array.c

--- array.c
+++ array.c
@@ -370,3 +370,29 @@ void main()
 
     return 0;
 }
+
+#Program to initialize 20 integers in a 4x5 matrix and print the numbers in matrix format
+
+output:-
+	
+12345
+54321
+12345
+54321
+
+#include <stdio.h>
+int main()
+{
+    int n[4][5]= {{1,2,3,4,5},
+                 {5,4,3,2,1},
+                 {1,2,3,4,5},
+                 {5,4,3,2,1}};
+    int i,j;
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<5;j++)
+        printf("%d",n[i][j]);
+        printf("\n");
+    }
+    return 0;
+}	
