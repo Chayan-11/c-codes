f23f0f6699a22ab57460ef09c93cba6d8e702fd3
Chayan Debbarma <chayan.nit24@gmail.com>
2021-06-27 23:36:02 +0530

    Update Number Conversion.c

--- Number Conversion.c
+++ Number Conversion.c
@@ -37,3 +37,23 @@ int main()
 	for(j=i-1;j>=0;j--)
 	printf("%d",a[j]);
 }
+
+Number conversion from Octal to Decimal
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int f,p=0,i=0,a[100],j,n;
+	printf("Enter the octal number:- \n");
+	scanf("%d",&n);
+	while(n>=1)
+	{
+		a[i]=n%10;
+		n=n/10;
+		i++;
+	}
+	for(j=0;j<i;j++)
+	p=p+(a[j]*pow(8,j));
+	printf("The equivalent decimal number is:- %d",p);
+}
+
