a16268a69f6debf94338261ae027ee8012c903f4
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-01 23:11:27 +0530

    Update Strings.c

--- Strings.c
+++ Strings.c
@@ -417,3 +417,18 @@ int main()
     }
        return 0;
 }    
+
+#Program to concatanate two different strings
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char n[]={"Chayan"};
+	char m[100];
+	printf("Enter any string:- ");
+	scanf("%s",m);
+	strcat(n,m);
+	strcat(n,"Debbarma");
+	printf("%s",n);
+}
