baf73dcd9dca88f8a5b320967143073ccfcda37e
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-02 23:40:10 +0530

    Update Basic Mathematics.c

--- Basic Mathematics.c
+++ Basic Mathematics.c
@@ -34,3 +34,32 @@ int main()
     r=r*10+i%10;
    printf("Reverse of a number=%d",r);
 }
+
+Program to find the roots of quadratic equation
+
+#include<stdio.h>
+void main()
+{
+  int a,b,c,d;
+  float r1,r2;
+  printf("Enter coefficients of three integers:");
+  scanf("%d%d%d",&a,&b,&c);
+  d=b*b-4*a*c;
+  if(d<0)
+  {
+    printf("Roots are real and unequal");
+    r1=(-b+sqrt(d))/(2*a);
+    r2=(-b-sqrt(d))/(2*a);
+    printf("r1=%f,r2=%f",r1,r2);
+  }
+  else if(d==0)
+  {
+    printf("Roots are real and equal");
+    r1=r2=(-b)/(2*a);
+    printf("r1=%f,r2=%f",r1,r2);
+  }
+  else
+  {
+    printf("Roots are imaginary and can not be found");
+  }
+}
