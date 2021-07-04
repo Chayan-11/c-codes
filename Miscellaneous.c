6c08c77e58f70539f6436e050ae00404b6a1fdcd
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-04 23:29:21 +0530

    Update Miscellaneous.c

--- Miscellaneous.c
+++ Miscellaneous.c
@@ -15,3 +15,19 @@ What are Modifiers in c?
 ➡ Modifier is a prefix to the basic datatype which is used to indicate the modification of storage space. 
   In a 32-bit processor, the storage space for int is 4 byte, but for long int the storage space is 8 byte and for short int it is 2 bytes. 
   There are 5 modifiers in c namely:- short, long, signed, unsigned, long long.   
+
+    
+Program to generate 10 random integers 
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a,b;                    //b[10];
+	for(a=1;a<11;a++)          //a=0; a<10;
+	{
+		b=rand();              //b[a];
+		printf("%d\n",b);     //b[a];  
+	}
+	return 0;
+}
