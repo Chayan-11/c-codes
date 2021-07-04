b0cb68ff7a5915de0cffb04be1d77109293c1526
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-04 22:33:16 +0530

    Update Advanced array.c

--- Advanced array.c
+++ Advanced array.c
@@ -80,3 +80,49 @@ int main()
 	printf("%.2f %d %.2f\n",*(q+i),*(p+i),*(r+i));
 	
 }	
+
+Program to find the number of duplicate elements in an array
+
+#include<stdio.h>
+int main()
+{
+	int arr1[100];
+	int arr2[100];
+	int arr3[100];
+	int n,m=1,ctr=0;
+	int i,j;
+	printf("Input number of elements:- \n");
+	scanf("%d",&n);
+	printf("Input %d elements in array:- \n",n);
+	for(i=0;i<n;i++)
+	{
+		printf("Element %d:- ",i);
+		scanf("%d",&arr1[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		arr2[i] = arr1[i];
+		arr3[i] = 0;
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(arr1[i] == arr2[j])
+			{
+				arr3[j] = m;
+				m++;
+			}
+		}
+		m=1;
+	}
+	for(i=0;i<n;i++)
+	{
+		if(arr3[i] == 2)
+		{
+			ctr++;
+		}
+	}
+	printf("Total number of duplicate elements found in array is:- %d\n",ctr);
+	return 1;
+}
