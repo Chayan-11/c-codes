db9fb705971684c55fee2bafe61536c27ba9bce3
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-04 23:02:47 +0530

    Create Miscellaneous.c

---
+++ Miscellaneous.c
@@ -0,0 +1,17 @@
+Explain prototype function in c.
+  
+➡ Prototype is nothing but an indication to the compiler at in the test of body is define with the following information to the compiler:- 
+  i) Name of the function
+  ii) Return type of the function
+  iii) List of the parameters of the function
+  
+What is cyclic nature of data type?
+
+➡ Some of the data types containing special characteristics while assigning values beyond the range of data types. No compiler error will occur 
+for run, this will occur in cyclic order for only char, int, long int datatypes.
+  
+What are Modifiers in c?
+  
+➡ Modifier is a prefix to the basic datatype which is used to indicate the modification of storage space. 
+  In a 32-bit processor, the storage space for int is 4 byte, but for long int the storage space is 8 byte and for short int it is 2 bytes. 
+  There are 5 modifiers in c namely:- short, long, signed, unsigned, long long.   
