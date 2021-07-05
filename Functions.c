45eaee1ae961900766a56cb384991e9159bd7003
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-05 22:59:07 +0530

    Update Functions.c

--- Functions.c
+++ Functions.c
@@ -591,3 +591,22 @@ int main()
      
     return 0;
 }
+
+Program to reverse a number using functions
+
+#include<stdio.h>
+int reverse(int num,int i,int rev);
+void main()
+{
+  int num,i,rev=0;
+  printf("Enter a number:");
+  scanf("%d",&num);
+  int result=reverse(num,i,rev);
+  printf("The reversed number=%d",result);
+}
+int reverse(int num,int i,int rev)
+{
+  for(i=num;i>0;i=i/10)
+    rev=rev*10+i%10;
+  return rev;
+}
