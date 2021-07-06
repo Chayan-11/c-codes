d80428a204b4253d6401fe3111066fd0814d11da
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-06 23:53:27 +0530

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -31,3 +31,23 @@ int main()
     printf("%d\n", i);
     return 0;
 }
+
+Program to add two numbers using pointers
+
+#include <stdio.h>
+int main()
+{
+   int f,s,*p,*q,sum;
+   
+   printf("\n\n Pointer : Add two numbers :\n"); 
+   printf("--------------------------------\n");   
+   printf(" Input the first number : ");
+   scanf("%d", &f);
+   printf(" Input the second  number : ");
+   scanf("%d", &s);  
+   p = &f;
+   q = &s;
+   sum = *p + *q;
+   printf(" The sum of the entered numbers is : %d\n\n",sum);
+   return 0;
+}
