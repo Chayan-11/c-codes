6c396f1eb573a05fb550389ad9366f4927bb5b02
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-09 22:57:01 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -152,3 +152,28 @@ int main() {
     cout << longestWordInSentence(x);
     return 0;
 }
+
+7) Program to count all the vowels in a given string
+
+#include <iostream>
+using namespace std;
+int Vowel_Count(string text) {
+  int ctr = 0;
+  for(int i = 0; i < int(text.size()); i++){
+    if (text[i] == 'a' || text[i] == 'e' || text[i] == 'i' || text[i] == 'o' || text[i] == 'u')
+      ++ctr;
+    if (text[i] == 'A' || text[i] == 'E' || text[i] == 'I' || text[i] == 'O' || text[i] == 'U')
+      ++ctr;
+  }
+  return ctr;
+
+}
+
+int main() {
+        cout << "Original string: eagerer, number of vowels -> " << Vowel_Count("eagerer") << endl;
+        cout << "\nOriginal string: eaglets, number of vowels -> " << Vowel_Count("eaglets") << endl;
+        cout << "\nOriginal string: w3resource, number of vowels -> " << Vowel_Count("w3resource") << endl;
+        cout << "\nOriginal string: After eagling the Road Hole on Thursday, he missed an 8-footer for birdie Friday., number of vowels -> ";
+		cout << Vowel_Count("After eagling the Road Hole on Thursday, he missed an 8-footer for birdie Friday.") << endl;
+        return 0;
+}
