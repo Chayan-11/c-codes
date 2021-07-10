1c2444a5d604e856d350b87bfc43228f6d44b594
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-10 23:52:30 +0530

    Update Basic problems.cpp

--- Basic problems.cpp
+++ Basic problems.cpp
@@ -26,3 +26,26 @@ int main()
      printf("%dx%d=%d\n",n,i,n*i);
      return 0;
 }
+
+Program to swap numbers without using temporary variable
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a = 5, b = 10;
+
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
