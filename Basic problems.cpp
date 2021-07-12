f90ffc4adf3305c2fea9509b673d4544764b4c5b
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-12 23:10:43 +0530

    Update Basic problems.cpp

--- Basic problems.cpp
+++ Basic problems.cpp
@@ -49,3 +49,28 @@ int main()
 
     return 0;
 }
+
+Program to check whether given number is prime or not
+	
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n)
+{
+
+	if (n <= 1)
+		return false;
+
+	for (int i = 2; i < n; i++)
+		if (n % i == 0)
+			return false;
+
+	return true;
+}
+
+int main()
+{
+	isPrime(11) ? cout << " true\n" : cout << " false\n";
+	isPrime(15) ? cout << " true\n" : cout << " false\n";
+	return 0;
+}
