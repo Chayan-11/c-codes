56858842c24813e4f2fa7f03b9d32f408b1bacc7
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-13 23:51:25 +0530

    Update Basic problems.cpp

--- Basic problems.cpp
+++ Basic problems.cpp
@@ -151,3 +151,22 @@ int main()
     cout<<endl;
     return 0;
 }
+
+Program to find sum of digits of a number using While loop
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num, rem, sum=0;
+    cout<<"Enter the Number: ";
+    cin>>num;
+    while(num>0)
+    {
+        rem = num%10;
+        sum = sum+rem;
+        num = num/10;
+    }
+    cout<<"\nSum of Digits = "<<sum;
+    cout<<endl;
+    return 0;
+}
