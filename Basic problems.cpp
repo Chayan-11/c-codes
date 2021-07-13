c400c64f6320206071b7fc460be4a693032366df
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-13 23:49:07 +0530

    Update Basic problems.cpp

--- Basic problems.cpp
+++ Basic problems.cpp
@@ -132,3 +132,22 @@ int main()
     cout<<endl;
     return 0;
 }
+
+Program to check whether given year is leap year or not
+	
+#include<iostream>
+using namespace std;
+int main()
+{
+    int yr;
+    cout<<"Enter the Year: ";
+    cin>>yr;
+    if((yr%4==0) && (yr%100!=0))
+        cout<<"\nIt is a Leap Year";
+    else if(yr%400==0)
+        cout<<"\nIt is a Leap Year";
+    else
+        cout<<"\nIt is not a Leap Year";
+    cout<<endl;
+    return 0;
+}
