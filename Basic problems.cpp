603de9999c7a8a31b9b8422c8bd915a9314b85bd
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-13 23:42:45 +0530

    Update Basic problems.cpp

--- Basic problems.cpp
+++ Basic problems.cpp
@@ -74,3 +74,27 @@ int main()
 	isPrime(15) ? cout << " true\n" : cout << " false\n";
 	return 0;
 }
+
+Program Given Number is equal to its Reverse or Not
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num, orig, rev=0, rem;
+    cout<<"Enter the Number: ";
+    cin>>num;
+    orig = num;
+    while(num>0)
+    {
+        rem = num%10;
+        rev = (rev*10)+rem;
+        num = num/10;
+    }
+    if(orig==rev)
+        cout<<"\nThis Number is equal to its Reverse";
+    else
+        cout<<"\nThis Number is not equal to its Reverse";
+    cout<<endl;
+    return 0;
+}
