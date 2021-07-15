ad6d4345727751e05cc1dff3a3e80d752cafcda6
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-15 23:50:35 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -200,3 +200,21 @@ int main()
     cout<<endl;
     return 0;
 }
+
+Program to print the ASCII value of all the characters 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    int i;
+    cout<<"Character\t\tASCII Value\n";
+    for(i=0; i<255; i++)
+    {
+        ch = i;
+        cout<<ch<<"\t\t\t"<<i<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
