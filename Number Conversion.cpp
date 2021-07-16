4d76112ca1e70e8314a4c0867e8549d2cb7c70a2
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-16 23:28:46 +0530

    Update Number Conversion.cpp

--- Number Conversion.cpp
+++ Number Conversion.cpp
@@ -19,3 +19,25 @@ int main()
     cout<<endl;
     return 0;
 }
+
+Program to convert Decimal number to Octal number 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int d_n,o_n[50],i=0;
+    cout<<"Enter any Decimal number:- \n";
+    cin>>d_n;
+    while(d_n!=0)
+    {
+        o_n[i]=d_n%8;
+        i++;
+        d_n=d_n/8;
+    }
+    cout<<"\nEquivalent Octal Value = ";
+    for(i=(i-1);i>=0;i--)
+        cout<<o_n[i];
+    cout<<endl;
+    return 0;
+}
