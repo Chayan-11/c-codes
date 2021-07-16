89b2ba20f9706adf768020afabbd41916a4cee97
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-16 23:44:14 +0530

    Create Pattern.cpp

---
+++ Pattern.cpp
@@ -0,0 +1,23 @@
+Print the half-pyramid pattern of stars
+
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * * *
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=0;i<6;i++)
+    {
+        for(j=0;j<=i;j++)
+            cout<<"* ";
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
