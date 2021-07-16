19494c218214931604eb9264ef3ba301a385a3ee
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-16 23:53:53 +0530

    Update Pattern.cpp

--- Pattern.cpp
+++ Pattern.cpp
@@ -45,3 +45,33 @@ int main()
     cout<<endl;
     return 0;
 }
+
+Print the Full Pyramid pattern using stars
+
+          *
+        * * *
+      * * * * *
+    * * * * * * *
+  * * * * * * * * *
+* * * * * * * * * * *
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,s,k=0;
+    for(i=1;i<=6;i++)
+    {
+        for(s=1;s<=(6-i);s++)
+            cout<<"  ";
+        while(k!=(2*i-1))
+        {
+            cout<<"* ";
+            k++;
+        }
+        k=0;
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
