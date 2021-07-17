510f4271dc11e7e7706c68191dee829b9aea7f61
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-17 23:52:31 +0530

    Update Pattern.cpp

--- Pattern.cpp
+++ Pattern.cpp
@@ -95,3 +95,30 @@ int main()
     return 0;
 }
 
+Print the inverted full pyramid pattern using stars
+
+* * * * * *
+ * * * * *
+  * * * *
+   * * *
+    * *
+     *
+     
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,s,j;
+    for(i=1;i<=6;i++)
+    {
+        for(s=1;s<i;s++)
+            cout<<" ";
+        for(j=i;j<=6;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}    
