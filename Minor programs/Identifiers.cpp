eb15e4f9e886350951fd929358ce7c9f20a45317
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-21 23:44:08 +0530

    Create Identifiers.cpp

---
+++ Minor programs/Identifiers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int minutesPerHour = 60;
+  int m = 60;
+  cout << minutesPerHour << "\n";    //demonstrate identifiers
+  cout << m;
+  return 0;
+}
