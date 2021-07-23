5586b8a758b0333ed3b5729727e149cb55d53558
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-23 22:25:41 +0530

    Create sum of numbers.cpp

---
+++ Functions/sum of numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void addition(int a, int b, int c){
+cout << "The sum of the given numbers = "<< a+b+c;
+}
+int main ()
+{
+int a = 1;
+int b = 4;
+int c = 3;
+addition(a,b,c);
+}
