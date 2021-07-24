f827a08b95f29bdf51e61fea4528b8dacaf193b0
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-25 00:25:07 +0530

    Update printing in forward and reverse direction.c

--- Dynamic Memory Allocation/printing in forward and reverse direction.c
+++ Dynamic Memory Allocation/printing in forward and reverse direction.c
@@ -1,1 +1,26 @@
+Program to input n integers and print them in forward and reverse direction using DMA
 
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n,i,*p;
+	printf("How many numbers? \n");
+	scanf("%d",&n);
+	p=(int*)malloc(n*sizeof(int));
+	if(p==NULL)
+	{
+		printf("Unable to allocate memory");
+		return 0;
+	}
+	for(i=0; i<n; i++)
+	{
+		printf("Enter Integers:- \n");
+		scanf("%d",p+i);
+	}
+	for(i=0; i<n; i++)
+	printf("%d\n",*(p+i));
+	for(i=n-1; i>=0; i--)
+	printf("%d\n",*(p+i));
+	free(p);
+}
