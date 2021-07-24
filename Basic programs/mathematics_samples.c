18d0594590d418cf7d5ed368be0d26d11f931adb
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-25 03:03:17 +0530

    Update mathematics_samples.c

--- Basic programs/mathematics_samples.c
+++ Basic programs/mathematics_samples.c
@@ -63,3 +63,35 @@ void main()
     printf("Roots are imaginary and can not be found");
   }
 }
+
+
+Strong number:- A number is said to be strong number when the sum of the factorial of the individual digits is equal to the number. e.g:- 145 is a strong number.
+    
+Program to print strong number in given range
+
+#include<stdio.h>
+int main()
+{
+int n,i,fact,r,sum=0,temp;
+printf("Enter the number:- \n");
+scanf("%d",&n);
+temp = n;
+while(n)
+{
+i=1,fact=1;
+r=n%10;
+while(i<=r)
+{
+fact=fact*i;
+i++;
+}
+sum=sum+fact;
+n=n/10;
+}
+if(sum == temp)
+printf("%d is a strong number\n",temp);
+else
+printf("%d is not a strong number\n",temp);
+return 0;
+}
+    
