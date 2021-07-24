60e0b6aacd845bf66422bfe19ff27eba627d4150
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-25 01:40:44 +0530

    Create multiplication table using DMA.c

---
+++ Dynamic Memory Allocation/multiplication table using DMA.c
@@ -0,0 +1,26 @@
+Input n integers and print the multiplication table of each integer using DMA
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i,j,n,*p;
+    printf("Enter the total number of elements you want to enter:- \n");
+    scanf("%d", &n);
+    p = (int*)malloc(n*sizeof(int));
+    if(p==NULL){
+    printf("Insufficient Memory Existing");
+    return 0;
+}
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter the Integers:- \n");
+        scanf("%d", p+i);
+    }
+    for(i=0; i<n; i++)
+    {
+    	for(j=1; j<=10; j++)
+    	printf("%dx%d=%d\t\n",*(p+i),j,*(p+i)*j);
+	}
+    free(p);
+}
