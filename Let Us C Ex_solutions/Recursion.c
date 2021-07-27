f35ca02d7de659330b5bbc96bc6d1a66bdecfa7e
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-27 22:00:00 +0530

    Create Recursion.c

---
+++ Let Us C Ex_solutions/Recursion.c
@@ -0,0 +1,31 @@
+// Output to the following snippet:- 
+
+#include<stdio.h>
+int main()
+{
+	printf("C to it that C survives\n");
+	main();
+	return 0;
+}
+
+Output:- Prints C to it that C survives Infinite times in the screen with return value 3221225725
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Output to the following snippet:- 
+
+#include<stdio.h>
+int main()
+{
+	int i=0;
+	i++;
+	if(i<=5)
+	{
+		printf("C adds wings to your thoughts\n");
+		exit(0);
+		main();
+	}
+	return 0;
+}
+
+Output:- C adds wings to your thoughts
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
