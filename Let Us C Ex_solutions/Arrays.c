a21839314704c27ceca989886a91a16db2138bb4
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-30 01:21:30 +0530

    Update Arrays.c

--- Let Us C Ex_solutions/Arrays.c
+++ Let Us C Ex_solutions/Arrays.c
@@ -67,3 +67,32 @@ int main()
 	}
 	return 0;
 }
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*Enter 25 numbers from the keyboard into an array. The number to be searched is to be entered by the user.
+Program to find if the number to be searched is either present in the array or not and if found then dispaly the number of times it appeared in the array.*/
+	
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int arr[25],a,i,count=0;
+	for (i=0; i<25; i++)
+	{
+	printf("Enter number :- \n");
+	scanf("%d", &arr[i]);
+	}
+	printf("\n\nEnter the number you want to search :- ");
+	scanf("%d", &a);
+	for (i=0; i<25; i++)
+	{
+	if (arr[i] == a)
+	count++;
+	}
+	if (count)
+	printf("\n\n%d is present in the data %d times.", a,count);
+	else
+	printf("\n\n%d is not present in the data.", a);
+	_getch();
+	return 0;
+}
