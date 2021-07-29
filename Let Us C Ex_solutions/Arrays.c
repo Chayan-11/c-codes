b66cad94692507d7b2e178d7aabbd4929719a6fd
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-30 00:56:01 +0530

    Update Arrays.c

--- Let Us C Ex_solutions/Arrays.c
+++ Let Us C Ex_solutions/Arrays.c
@@ -44,3 +44,26 @@ int main()
 	printArray(copied_arr, len);
 	return 0;
 }
+
+// OR
+
+#include<stdio.h>
+int main()
+{
+	int a[10], b[10], i, j;
+	printf("\nEnter 10 Elements : ");
+	for (i=0; i<10; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	for (i=0, j=9; i<10; i++, j--)
+	{
+		b[i] = a[j];
+	}
+	printf("\nArray after Copying in Reverse Order : ");
+	for (i=0; i<10; i++)
+	{
+		printf("%d ", b[i]);
+	}
+	return 0;
+}
