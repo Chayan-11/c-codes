16f1a261d802f6c2d509054767b0d814eb5f4d8b
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-30 23:48:54 +0530

    Create calling multiple functions.cpp

---
+++ Functions/calling multiple functions.cpp
@@ -0,0 +1,18 @@
+/* Program to call a function multiple times */
+
+#include<iostream>
+using namespace std;
+void myfunction()
+{
+	cout << "I am learning how to create functions\n";
+}
+
+int main()
+{
+	myfunction();
+	myfunction();
+	myfunction();
+	myfunction();
+	myfunction();
+	return 0;
+}
