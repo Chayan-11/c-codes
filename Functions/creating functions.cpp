4734575750c7b64bf9b78e80d472ac4e4ca6dfee
Chayan Debbarma <chayan.nit24@gmail.com>
2021-07-30 23:45:40 +0530

    Create creating functions.cpp

---
+++ Functions/creating functions.cpp
@@ -0,0 +1,14 @@
+/*Program to create and call a function*/
+
+#include<iostream>
+using namespace std;
+void myfunction()
+{
+	cout << "I am learning how to create functions";
+}
+
+int main()
+{
+	myfunction();
+	return 0;
+}
