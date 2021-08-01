7faf73dde86eaac6d89265cc97be4d274c0a554a
Chayan Debbarma <chayan.nit24@gmail.com>
2021-08-02 01:40:56 +0530

    Create returning values.cpp

---
+++ Functions/returning values.cpp
@@ -0,0 +1,16 @@
+//Program to return values in function
+
+#include <iostream>
+using namespace std;
+int new_function(int n) 
+{
+  return 5 + n;
+}
+
+int main() 
+{
+  cout << new_function(3);
+  return 0;
+}
+
+// output:- 8
