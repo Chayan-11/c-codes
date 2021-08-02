b90dd84faf310b76df331d6f5381df7632e7fe4e
Chayan Debbarma <chayan.nit24@gmail.com>
2021-08-02 23:51:40 +0530

    Create demonstrate boolean.cpp

---
+++ Boolean/demonstrate boolean.cpp
@@ -0,0 +1,12 @@
+//Program to demonstrate boolean values
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	bool IsCodingFun = true;
+	bool CsIsBoring = false;
+	cout << IsCodingFun <<"\n";
+	cout << CsIsBoring;
+	return 0;
+}
