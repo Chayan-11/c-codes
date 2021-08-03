2753e9046bb99da8710330e6dde1be815b888b57
Chayan Debbarma <chayan.nit24@gmail.com>
2021-08-04 00:26:43 +0530

    Update Arrays.c

--- Let Us C Ex_solutions/Arrays.c
+++ Let Us C Ex_solutions/Arrays.c
@@ -139,3 +139,23 @@ int main()
 	}
 		return 0;
 }
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+/* Program to use pointers and find the smallest number in an array of 25 integers */
+	
+#include<stdio.h>
+int main()
+{
+	int arr[25],i,n;
+	printf("Enter 25 elements in an array:- \n");
+	for(i=0; i<=24; i++)
+	scanf("%d",&arr[i]);
+	n=*arr;
+	for(i=0; i<=24; i++)
+	{
+		if(*(arr+i)<n)
+		n=*(arr+i);
+	}
+	printf("Smallest number in the array is:- %d\n",n);
+	return 0;
+}
