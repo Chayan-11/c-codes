69bf149d90999d215ce8e1a8866875b9dead1b9b
Chayan Debbarma <chayan.nit24@gmail.com>
2021-08-10 23:39:31 +0530

    Update Recursion.c

--- Let Us C Ex_solutions/Recursion.c
+++ Let Us C Ex_solutions/Recursion.c
@@ -8,7 +8,7 @@ int main()
 	return 0;
 }
 
-Output:- Prints C to it that C survives Infinite times in the screen with return value 3221225725
+// Output:- Prints C to it that C survives Infinite times in the screen with return value 3221225725
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //Output to the following snippet:- 
@@ -27,5 +27,32 @@ int main()
 	return 0;
 }
 
-Output:- C adds wings to your thoughts
+// Output:- C adds wings to your thoughts
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//A positive integer is entered through the keyboard, write a function to find the binary equivalent of this number using recursive function
+
+#include<stdio.h>
+int binary(int); //function prototype declaration
+int main()
+{
+	int n;
+	printf("Enter any integer:- \n");
+	scanf("%d",&n);
+	binary(n);  //function call
+	return 0;
+}
+int binary(int n)
+{
+	int r;
+	r=n%2;
+	n=n/2;
+	if(n==0)
+	{
+		printf("The Binary equivalent of the decimal integer is :- %d",r);
+		return(r);
+	}
+		else
+		binary(n); //recursive call
+		printf("%d",r);
+}
